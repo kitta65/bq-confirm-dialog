8e3c356ac311277dc6406c8e4623115972f87eef
dr666m1 <skndr666m1@gmail.com>
2024-01-06 21:58:14 +0900

    define Cost class

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,18 @@
-import { expect, test } from "vitest";
-import { extractCost } from "./utils";
+import { expect, test, describe } from "vitest";
+import { Cost } from "./utils";
 
-test("", () => {
-  expect(extractCost("5")).toBe(5);
+describe("extractCost", () => {
+  test("bytes", () => {
+    expect(new Cost("200 B").bytes).toBe(200);
+  });
+
+  test("mega bytes", () => {
+    expect(new Cost("5 MB").bytes).toBe(5000000);
+  });
+
+  test("validation status", () => {
+    expect(new Cost("This query will process 36.48 MB when run.").bytes).toBe(
+      36480000
+    );
+  });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -14,7 +14,35 @@ export function markAsProcessed(elm: Element) {
   elm.setAttribute("bq-confirm-dialog", "processed");
 }
 
-// const re = /\d(\.\d+)?\s*.B/
-export function extractCost(state: string): Number {
-  return Number(state);
+const re = /(\d+(\.\d+)?)\s*(\S?)B/;
+export class Cost {
+  text: string;
+  bytes: number;
+
+  constructor(text: string) {
+    const matched = text.match(re);
+    if (!matched) {
+      throw new Error("cannot extract cost");
+    }
+    this.text = matched[0];
+    let bytes = Number(matched[1]);
+    switch (matched[3]) {
+      case "K":
+        bytes *= 1000;
+        break;
+      case "M":
+        bytes *= 1000 ** 2;
+        break;
+      case "G":
+        bytes *= 1000 ** 3;
+        break;
+      case "T":
+        bytes *= 1000 ** 4;
+        break;
+      case "P":
+        bytes *= 1000 ** 5;
+        break;
+    }
+    this.bytes = bytes;
+  }
 }
